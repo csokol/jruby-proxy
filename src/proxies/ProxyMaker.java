7a97ef146eb9924c47c1d088668f3bec64fc62fa
Francisco Sokol <chico.sokol@gmail.com>
2013-01-10 12:19:21 -0200

    refactoring

--- src/proxies/ProxyMaker.java
+++ src/proxies/ProxyMaker.java
@@ -15,30 +15,34 @@ import javassist.util.proxy.ProxyObject;
 public class ProxyMaker<T> {
 
 	private final Class<T> classToProxy;
+	private MethodFilter allMethods;
 
 	public ProxyMaker(Class<T> classToProxy) {
 		this.classToProxy = classToProxy;
+		this.allMethods = new AllMethods(); 
 	}
 
 	@SuppressWarnings("unchecked")
-	public T proxyAllMethods(MethodHandler methodHandler) {
+	public T proxyAllMethods(MethodHandler handlerGiven) {
 		try {
 			ProxyFactory proxyFactory = new ProxyFactory();
 			proxyFactory.setSuperclass(classToProxy);
-			proxyFactory.setFilter(new MethodFilter() {
-				@Override
-				public boolean isHandled(Method m) {
-					return true;
-				}
-			});
+			proxyFactory.setFilter(allMethods);
 			Class<T> proxiedClass = proxyFactory.createClass();
 			Object proxy = proxiedClass.newInstance();
-			((ProxyObject) proxy).setHandler(methodHandler);
+			((ProxyObject) proxy).setHandler(handlerGiven);
 			
 			return (T) proxy;
 		} catch (InstantiationException | IllegalAccessException e) {
 			throw new RuntimeException(e);
 		}
 	}
+	
+	private class AllMethods implements MethodFilter {
+		@Override
+		public boolean isHandled(Method m) {
+			return true;
+		}
+	};
 
 }
